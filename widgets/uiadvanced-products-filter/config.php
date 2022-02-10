037fc69e67bddc588d5264c66d839dd053e9404e
Tuyen <tuyennv@templaza.com>
2022-02-10 12:08:19 +0700

    add fields ad products filter

--- widgets/uiadvanced-products-filter/config.php
+++ widgets/uiadvanced-products-filter/config.php
@@ -49,6 +49,74 @@ if ( ! class_exists( 'UIPro_Config_UIAdvanced_Products_Filter' ) ) {
 			// options
 			$options = array(
                 array(
+                    'type'          => Controls_Manager::TEXT,
+                    'name'          => 'title',
+                    'label'         => esc_html__( 'Title', 'uipro' ),
+                    'default'       => esc_html__('Inventory Search', 'uipro'),
+                    'description'   => esc_html__( 'Write the title for Search form.', 'uipro' ),
+                ),
+                array(
+                    'type'          => Controls_Manager::SELECT,
+                    'name'          => 'title_display',
+                    'label'         => esc_html__( 'Title display', 'uipro' ),
+                    'options'       => array(
+                        'uk-display-inline'       => esc_html__( 'Inline', 'uipro' ),
+                        'uk-display-block'        => esc_html__( 'Block', 'uipro' ),
+                        'uk-display-inline-block' => esc_html__( 'Inline Block', 'uipro' ),
+                    ),
+                    'default'       => 'uk-display-block',
+                    'condition'     => array(
+                        'title!'    => ''
+                    ),
+                ),
+                array(
+                    'type'          => Controls_Manager::DIMENSIONS,
+                    'name'          =>  'title_padding',
+                    'label'         => esc_html__( 'Title Padding', 'uipro' ),
+                    'responsive'    =>  true,
+                    'size_units'    => [ 'px', 'em', '%' ],
+                    'selectors'     => [
+                        '{{WRAPPER}} .inventory-title-search' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                    ],
+                    'condition'     => array(
+                        'title!'    => ''
+                    ),
+                ),
+                array(
+                    'type'          => Controls_Manager::DIMENSIONS,
+                    'name'          =>  'title_margin',
+                    'label'         => esc_html__( 'Title Margin', 'uipro' ),
+                    'responsive'    =>  true,
+                    'size_units'    => [ 'px', 'em', '%' ],
+                    'selectors'     => [
+                        '{{WRAPPER}} .inventory-title-search' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                    ],
+                    'condition'     => array(
+                        'title!'    => ''
+                    ),
+                ),
+                array(
+                    'type'          => Controls_Manager::SELECT,
+                    'name'          => 'title_tag',
+                    'label'         => esc_html__( 'Title tag', 'uipro' ),
+                    'options'       => array(
+                        'h1'        => 'h1',
+                        'h2'        => 'h2',
+                        'h3'        => 'h3',
+                        'h4'        => 'h4',
+                        'h5'        => 'h5',
+                        'h6'        => 'h6',
+                        'div'       => 'div',
+                        'span'      => 'span',
+                        'p'         => 'p',
+                    ),
+                    'default'       => 'h3',
+                    'description'   => esc_html__( 'Choose heading element.', 'uipro' ),
+                    'condition'     => array(
+                        'title!'    => ''
+                    ),
+                ),
+                array(
                     'type'          => Controls_Manager::SELECT2,
                     'id'            => 'uiap_custom_fields',
                     'label'         => esc_html__( 'Select Custom Field', 'uipro' ),
@@ -60,6 +128,69 @@ if ( ! class_exists( 'UIPro_Config_UIAdvanced_Products_Filter' ) ) {
                     'id'            => 'uiap_enable_keyword',
                     'label'         => esc_html__( 'Filter By Keyword', 'uipro' ),
                     'default'       => 'yes'
+                ),
+                array(
+                    'type'          => Controls_Manager::DIMENSIONS,
+                    'name'          =>  'form_padding',
+                    'label'         => esc_html__( 'Form Padding', 'uipro' ),
+                    'responsive'    =>  true,
+                    'size_units'    => [ 'px', 'em', '%' ],
+                    'selectors'     => [
+                        '{{WRAPPER}} .advanced-product-search-form' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                    ],
+                ),
+                array(
+                    'name'            => 'form_border',
+                    'type'          =>  \Elementor\Group_Control_Border::get_type(),
+                    'label' => esc_html__( 'Form Border', 'uipro' ),
+                    'selector' => '{{WRAPPER}} .advanced-product-search-form',
+                ),
+                array(
+                    'type'          => Group_Control_Typography::get_type(),
+                    'name'          => 'title_typography',
+                    'scheme'        => Typography::TYPOGRAPHY_1,
+                    'label'         => esc_html__('Title Font', 'uipro'),
+                    'description'   => esc_html__('Select a font family, font size for the addon title.', 'uipro'),
+                    'selector'      => '{{WRAPPER}} .inventory-title-search',
+                    'condition'     => array(
+                        'title!'    => ''
+                    ),
+                    'start_section' => 'style',
+                    'section_tab'   => Controls_Manager::TAB_STYLE,
+                    'section_name'  => esc_html__( self::$name, 'uipro' ),
+                ),
+                array(
+                    'type'          =>  Controls_Manager::COLOR,
+                    'name'          => 'title_color',
+                    'label'         => esc_html__('Title Color', 'uipro'),
+                    'description'   => esc_html__('Set the color of title.', 'uipro'),
+                    'selectors' => [
+                        '{{WRAPPER}} .inventory-title-search' => 'color: {{VALUE}}',
+                    ],
+                    'condition'     => array(
+                        'title!'    => ''
+                    ),
+                ),
+                array(
+                    'type'          =>  Controls_Manager::COLOR,
+                    'name'          => 'title_bg_color',
+                    'label'         => esc_html__('Title background Color', 'uipro'),
+                    'description'   => esc_html__('Set the background color of title.', 'uipro'),
+                    'selectors' => [
+                        '{{WRAPPER}} .inventory-title-search' => 'background-color: {{VALUE}}',
+                    ],
+                    'condition'     => array(
+                        'title!'    => ''
+                    ),
+                ),
+                array(
+                    'type'          =>  Controls_Manager::COLOR,
+                    'name'          => 'form_bg_color',
+                    'label'         => esc_html__('Form background Color', 'uipro'),
+                    'description'   => esc_html__('Set the background color of form.', 'uipro'),
+                    'selectors' => [
+                        '{{WRAPPER}} .advanced-product-search-form' => 'background-color: {{VALUE}}',
+                    ],
                 ),
 			) ;
 			return array_merge($options, $this->get_general_options());
--- widgets/uiadvanced-products-filter/tpl/base.php
+++ widgets/uiadvanced-products-filter/tpl/base.php
@@ -1,7 +1,7 @@
 <?php
 
 defined( 'UIPRO' ) || exit;
-
+use TemPlaza_Woo_El\TemPlaza_Woo_El_Helper;
 use Advanced_Product\Helper\AP_Custom_Field_Helper;
 
 // Get custom fields
@@ -10,6 +10,11 @@ $fields_include  = isset($instance['uiap_custom_fields'])?$instance['uiap_custom
 
 $enable_keyword = filter_var($enable_keyword, FILTER_VALIDATE_BOOLEAN);
 
+$general_styles = \UIPro_Elementor_Helper::get_general_styles($instance);
+$title          =   isset($instance['title']) ? $instance['title'] : '';
+$title_tag      =   isset($instance['title_tag']) ? $instance['title_tag'] : 'h3';
+$title_display      =   isset($instance['title_display']) ? $instance['title_display'] : 'uk-display-block';
+
 $shortcode  = '[advanced-product-form';
 if(!empty($fields_include) && count($fields_include)) {
     $shortcode .= ' include="'.implode(',', $fields_include).'"';
@@ -17,6 +22,12 @@ if(!empty($fields_include) && count($fields_include)) {
     $shortcode .= ' include=""';
 }
 $shortcode .= ' enable_keyword="'.($enable_keyword?1:0).'"]';
-
-do_shortcode($shortcode);
-
+?>
+<div class=" <?php echo esc_attr($general_styles['container_cls'] . $general_styles['content_cls']);?>" <?php echo wp_kses($general_styles['animation'],'post');?>>
+    <?php
+    if($title){
+    echo '<'.esc_html($title_tag).' class="inventory-title-search '.esc_html($title_display).'"> '.esc_html($title).'</'.esc_html($title_tag).'>';
+    }
+    do_shortcode($shortcode);
+    ?>
+</div>
