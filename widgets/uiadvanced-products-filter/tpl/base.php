f0bc05026d3d6dedfa6e467473d6f7de81e0a0b5
Tuyen <tuyennv@templaza.com>
2022-02-10 11:02:59 +0700

    add style account

--- widgets/uiadvanced-products-filter/tpl/base.php
+++ widgets/uiadvanced-products-filter/tpl/base.php
@@ -3,7 +3,6 @@
 defined( 'UIPRO' ) || exit;
 
 use Advanced_Product\Helper\AP_Custom_Field_Helper;
-
 // Get custom fields
 $enable_keyword  = isset($instance['uiap_enable_keyword'])?$instance['uiap_enable_keyword']:'yes';
 $fields_include  = isset($instance['uiap_custom_fields'])?$instance['uiap_custom_fields']:array();
@@ -19,4 +18,3 @@ if(!empty($fields_include) && count($fields_include)) {
     $shortcode .= '" enable_keyword="'.($enable_keyword?1:0).'"]';
     do_shortcode($shortcode);
 }
-
